f0634cfad45fd6c6cf09217f0bcabe1c2611ca8e
caizhangfeng <122385930@qq.com>
2022-09-20 18:36:08 +0800

    验证ssh连接问题

--- config/v1/config.go
+++ config/v1/config.go
@@ -2,39 +2,39 @@ package config
 
 import (
 	"fmt"
-	"gossh/utils"
+	"gossh/libs/configfile"
 	"os"
 	"path"
 )
 
-var userHomeDir, _ = os.UserHomeDir()
-
 var ProjectName = "GoSSH"
 
+// var userHomeDir, _ = os.UserHomeDir()
 // WorkDir 程序默认工作目录,在用户的home目录下 .GoSSH 目录
-var WorkDir = path.Join(userHomeDir, fmt.Sprintf("/.%s/", ProjectName))
+// var WorkDir = path.Join(userHomeDir, fmt.Sprintf("/.%s/", ProjectName))
+var WorkDir string
 
 // Config 默认配置,当配置文件不存在的时候,就使用这个默认配置
 var Config = map[string]map[string]string{
-	"app": {
-		"AppName": "GoSSH",
-	},
-	"server": {
-		"Address":  "0.0.0.0",
-		"Port":     "8899",
-		"CertFile": path.Join(WorkDir, "cert.pem"),
-		"KeyFile":  path.Join(WorkDir, "key.key"),
-	},
-	"session": {
-		"Secret":   utils.RandString(64),
-		"Name":     "session_id",
-		"Path":     "/",
-		"Domain":   "",
-		"MaxAge":   "86400",
-		"Secure":   "false",
-		"HttpOnly": "true",
-		"SameSite": "2",
-	},
+	// "app": {
+	// 	"AppName": "GoSSH",
+	// },
+	// "server": {
+	// 	"Address":  "0.0.0.0",
+	// 	"Port":     "8899",
+	// 	"CertFile": path.Join(WorkDir, "cert.pem"),
+	// 	"KeyFile":  path.Join(WorkDir, "key.key"),
+	// },
+	// "session": {
+	// 	"Secret":   utils.RandString(64),
+	// 	"Name":     "session_id",
+	// 	"Path":     "/",
+	// 	"Domain":   "",
+	// 	"MaxAge":   "86400",
+	// 	"Secure":   "false",
+	// 	"HttpOnly": "true",
+	// 	"SameSite": "2",
+	// },
 }
 
 const (
@@ -42,6 +42,8 @@ const (
 	FAILURE = 1
 )
 
-// func init() {
-// 	Config = configfile.Parse(path.Join(WorkDir, "GoSSH.cnf"))
-// }
+func init() {
+	WorkDir, _ := os.Getwd()
+	fmt.Println("WorkDir: ", WorkDir)
+	Config = configfile.Parse(path.Join(WorkDir, "GoSSH.cnf"))
+}
